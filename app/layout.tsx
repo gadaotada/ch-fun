3c4dcc13023bdc36bcb9f8e01db8c097aca7b5be
gadaotada <miro0o92@abv.bg>
2024-02-05 23:21:17 +0200

    test 38

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,15 +1,9 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
 import { Toaster } from 'react-hot-toast';
 import "./globals.css";
 
 import { cn } from "../lib/utils";
 import { ThemeProvider } from "../components/theme-provider"
-
-export const fontSans = Inter({
-  subsets: ["latin"],
-  variable: "--font-sans",
-})
 
 export const metadata: Metadata = {
   title: "Pushups Challenge",
@@ -24,8 +18,7 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning={true}>
       <body className={cn(
-          "min-h-screen bg-background font-sans antialiased",
-          fontSans.variable
+          "min-h-screen bg-background font-sans antialiased"
         )}
         >
           <ThemeProvider
